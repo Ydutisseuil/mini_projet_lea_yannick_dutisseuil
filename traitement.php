d0b289d053361562b2920fbefb4d77ee5f233bbf
yannick <dutisseuil1976@gmail.com>
2024-07-25 13:38:08 +0200

    modiff

--- traitement.php
+++ traitement.php
@@ -12,7 +12,7 @@ $mot_de_passe, $base_de_donnees);
 
 // Vérifier la connexion 
 if (!$connexion) { 
-     die("Échec de la connexion : " . mysqli_connect_error());
+    die("Échec de la connexion : " . mysqli_connect_error());
 } else {
     echo "Connexion réussie à la base de données.";
 
@@ -22,18 +22,18 @@ if (!$connexion) {
 // Vérification si le formulaire est soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-// Récupération des données du formulaire
-$nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
-$email = $_POST['email'];
-$temps_ecoute = isset($_POST['temps']) ? $_POST['temps'] : '';
+    // Récupération des données du formulaire
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $email = $_POST['email'];
+    $temps_ecoute = isset($_POST['temps']) ? $_POST['temps'] : '';
 
-// Requête d'insertion des données
-$sql = "INSERT INTO utilisateur (nom, prenom, email, temps_ecoute)
-VALUES ('$nom', '$prenom', '$email', '$temps_ecoute')";
+    // Requête d'insertion des données
+    $sql = "INSERT INTO utilisateur (nom, prenom, email, temps_ecoute)
+    VALUES ('$nom', '$prenom', '$email', '$temps_ecoute')";
 
-// Exécution de la requête d'insertion
-mysqli_query($connexion,$sql);
+    // Exécution de la requête d'insertion
+    mysqli_query($connexion,$sql);
 }
 header("Location: discographie.php");
 
